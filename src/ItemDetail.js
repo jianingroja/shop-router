00a0f0b86834979dffd7e1457b1d6355eb574b67
Jianing <jianingw321@outlook.com>
2020-10-24 13:47:15 +0800

    seperate ItemDetail page

---
+++ src/ItemDetail.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
+
+function ItemDetail() {
+  useEffect(() => {}, []);
+
+  const [items, setItems] = useState([]);
+
+  return <div>Item</div>;
+}
+
+export default ItemDetail;
--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Link } from "react-router-dom";
 
-function Shop() {
+function Item() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://fortnite-api.theapinetwork.com/upcoming/get"
+    );
+    const items = await data.json();
+    console.log(items.data);
+    setItems(items.data);
+  };
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map((item) => (
+        <h1 key={item.itemId}>
+          <Link to={`/shop/${item.itemId}`}> {item.item.name}</Link>
+        </h1>
+      ))}
     </div>
   );
 }
 
-export default Shop;
+export default Item;
